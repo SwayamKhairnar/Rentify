100fe4857a20cf66417141d329c68267009d1878
SwayamKhairnar <swayamkhairnar08@gmail.com>
2026-05-13 19:10:36 +0530

    Configured MongoDB Atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
 
 const createApp = require('./app');
 const connectDB = require('./config/db');
